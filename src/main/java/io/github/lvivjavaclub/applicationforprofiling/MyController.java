6a7d914664d53950baedb328163b9eacadbf4502
Ivan Verhun <902581+iverhun@users.noreply.github.com>
2019-09-05 10:41:08 +0300

    test

--- src/main/java/io/github/lvivjavaclub/applicationforprofiling/MyController.java
+++ src/main/java/io/github/lvivjavaclub/applicationforprofiling/MyController.java
@@ -1,11 +1,17 @@
 package io.github.lvivjavaclub.applicationforprofiling;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -14,6 +20,37 @@ public class MyController {
 
   HashMap STORAGE = new HashMap<String, Integer>(1, 0.000_9f);
   String CHARS = "0123456789ACEFGHJKLMNPQRUVWXYabcdefhijkprstuvwx";
+
+  @GetMapping("/snail")
+  @ResponseBody
+  public String snail() {
+    Random seed = new Random();
+    try {
+      Thread.sleep(seed.nextInt(5000));
+    } catch (InterruptedException e) {
+
+    }
+    return generate();
+  }
+
+
+  @GetMapping("/waitForSnail")
+  @ResponseBody
+  public String waitForSnail() {
+    OkHttpClient client = new OkHttpClient();
+
+      Request request = new Request.Builder()
+              .url("http://localhost:8080/snail")
+              .build();
+
+      try (Response response = client.newCall(request).execute()) {
+        return response.body().string();
+      } catch (IOException e) {
+
+      }
+
+    return "error";
+  }
 
   @GetMapping("/generate")
   @ResponseBody
@@ -56,9 +93,19 @@ public class MyController {
 
   @Scheduled(initialDelay = 2000, fixedDelay = 1000)
   public void initialDelay2() {
+    String result = "";
     for (int i = 0; i < 5; i++) {
-      generate();
+      result += generate();
     }
+
+    try {
+      File file = File.createTempFile("profiling-test", "txt");
+      System.out.println(Thread.currentThread().getName() + " - " + file.getAbsolutePath());
+      Files.write(file.toPath(), result.getBytes());
+    } catch (IOException e) {
+
+    }
+
     System.out.println("initialDelay2");
   }
 
